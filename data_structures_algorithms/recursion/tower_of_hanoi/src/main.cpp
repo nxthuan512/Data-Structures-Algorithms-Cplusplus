11ad65151370f093273db1aae722919b9d1212f0
Thomas (Thuan) Nguyen <nxthuan512@gmail.com>
2022-04-18 19:18:38 -0400

    Test with more disks.

--- data_structures_algorithms/recursion/tower_of_hanoi/src/main.cpp
+++ data_structures_algorithms/recursion/tower_of_hanoi/src/main.cpp
@@ -11,7 +11,7 @@ void move_disk(int n_disks, Stack stackA, Stack stackB, Stack stackC) {
 }
 
 int main() {
-   int n_disks = 4;
+   int n_disks = 6;
    for (int i = 1; i < n_disks; ++i) {
       std::cout << "====== N_DISKS = " << i << " ======" << std::endl;
       move_disk(i, stackA, stackB, stackC);
