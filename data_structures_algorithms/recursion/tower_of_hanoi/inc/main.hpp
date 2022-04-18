11d89b28743615c5b1ad0b59640748b1793468ae
Thomas (Thuan) Nguyen <nxthuan512@gmail.com>
2022-04-18 19:13:13 -0400

    Update solution.

---
+++ data_structures_algorithms/recursion/tower_of_hanoi/inc/main.hpp
@@ -0,0 +1,11 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <iostream>
+
+enum Stack {stackA, stackB, stackC};
+
+void move_disk(int n_disks, Stack stackA, Stack stackB, Stack stackC);
+
+
+#endif /* MAIN_HPP */
---
+++ data_structures_algorithms/recursion/tower_of_hanoi/src/main.cpp
@@ -0,0 +1,20 @@
+#include "main.hpp"
+
+void move_disk(int n_disks, Stack stackA, Stack stackB, Stack stackC) {
+   if (n_disks == 1) {
+      std::cout << "Move disk from " << stackA << " to " << stackC << std::endl;
+   } else {
+      move_disk(n_disks-1, stackA, stackC, stackB);
+      std::cout << "Move disk from " << stackA << " to " << stackC << std::endl;
+      move_disk(n_disks-1, stackB, stackA, stackC);
+   }
+}
+
+int main() {
+   int n_disks = 4;
+   for (int i = 1; i < n_disks; ++i) {
+      std::cout << "====== N_DISKS = " << i << " ======" << std::endl;
+      move_disk(i, stackA, stackB, stackC);
+   }
+   return 0;
+}
