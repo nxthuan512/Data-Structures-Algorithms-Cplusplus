f5c677f9ebf25242d026040f234dd2be4234d412
Thomas (Thuan) Nguyen <nxthuan512@gmail.com>
2022-06-02 00:09:01 -0400

    Refactor the code.

--- data_structures_algorithms/graph/inc/main.hpp
+++ data_structures_algorithms/graph/inc/main.hpp
@@ -1,6 +1,9 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+#include "Graph.hpp"
+
+Graph initialize_graph(int n_vertices);
 
 
 #endif /* MAIN_HPP */
--- data_structures_algorithms/graph/src/main.cpp
+++ data_structures_algorithms/graph/src/main.cpp
@@ -1,12 +1,19 @@
 #include "main.hpp"
-#include "Graph.hpp"
 
 int main() {
-   Graph simple_graph(5, 0);
+   Graph simple_graph = initialize_graph(5);
+   simple_graph.print_all();
+   return 0;
+}
+
+// =========================================
+Graph initialize_graph(int n_vertices) {
+   std::cout << "Initialize graph" << std::endl;
+   Graph simple_graph(n_vertices, 0);
    simple_graph.add_edge(0, 1);
    simple_graph.add_edge(0, 2);
    simple_graph.add_edge(0, 3);
    simple_graph.add_edge(1, 2);
-   simple_graph.print_all();
-   return 0;
+
+   return simple_graph;
 }
